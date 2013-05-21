ffc383d9056e97acb656d11082a4d65ed5976cf0
Allis Tauri <allista@gmail.com>
2013-05-21 16:33:42 +0400

    Changed version in setup.py and debian/changelog.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ rcc_file.close()
 #setup
 from distutils.core import setup
 setup(name='degen-primer-gui',
-      version='1.5',
+      version='2.0',
       description='Qt GUI for DegenPrimer',
       long_description=read('README'),
       license='GPL-3',
